7a4d41a957bfc340e9703a87ee0b5b29a5fa228a
Java_Program <ratul.mallick1998@gmail.com>
2023-09-29 12:42:16 +0530

    Factorial added

---
+++ src/Logical/Factorial.java
@@ -0,0 +1,15 @@
+package Logical;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		int temp = 1;
+		
+		for(int i = 1; i <= 4; i++) {
+			temp = temp * i;
+		}
+		
+		System.out.println(temp);
+	}
+}

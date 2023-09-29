7441e532de1b484be41bc47b47e91b00f4a3e496
Java_Program <ratul.mallick1998@gmail.com>
2023-09-29 13:02:36 +0530

    Add a Reverse String Program

---
+++ src/Logical/ReverseString.java
@@ -0,0 +1,18 @@
+package Logical;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+		
+		String name = "KCILLAM";
+		
+		String rev = "";
+		
+		for(int i = name.length()-1; i>=0; i--) {
+			rev = rev + name.charAt(i);
+		}
+		
+		System.out.println(rev);
+	}
+
+}
